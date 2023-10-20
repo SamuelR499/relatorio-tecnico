ce579e1244e8ecf39c503cd76234068d6eb42e22
SamuelR499 <samuelribeiro2@hotmail.com>
2023-10-20 17:53:03 -0300

    corrigindo erros de lint

--- src/Components/Orders.jsx
+++ src/Components/Orders.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable no-magic-numbers */
-/* eslint-disable sonarjs/no-duplicate-string */
-/* eslint-disable max-params */
+import PropTypes from 'prop-types';
 import * as React from 'react';
 import Link from '@mui/material/Link';
 import Table from '@mui/material/Table';
@@ -9,52 +7,11 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-
-const rows = [
-  createData(
-    0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
-  ),
-];
-
 function preventDefault(event) {
   event.preventDefault();
 }
 
-export default function Orders() {
+export default function Orders({ data }) {
   return (
     <>
       <Table size="small">
@@ -68,7 +25,7 @@ export default function Orders() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {data.map((row) => (
             <TableRow key={ row.id }>
               <TableCell>{row.date}</TableCell>
               <TableCell>{row.name}</TableCell>
@@ -85,3 +42,14 @@ export default function Orders() {
     </>
   );
 }
+
+Orders.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    date: PropTypes.string,
+    name: PropTypes.string,
+    shipTo: PropTypes.string,
+    paymentMethod: PropTypes.string,
+    amount: PropTypes.number,
+  })).isRequired,
+};
