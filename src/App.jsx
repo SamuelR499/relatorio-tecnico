08adbcbbb1cebb45dbf8f6c8163ea906424f2cc9
SamuelR499 <samuelribeiro2@hotmail.com>
2023-11-04 19:03:24 -0300

    Adicionar o tema no app inteiro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from '@mui/material';
 import AppRoutes from './Routes/AppRoutes';
+import { LightTheme } from './themes';
 
 function App() {
   return (
     <div className="App">
-      <AppRoutes />
+      <ThemeProvider theme={ LightTheme }>
+        <AppRoutes />
+      </ThemeProvider>
     </div>
   );
 }
